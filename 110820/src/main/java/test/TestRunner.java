c526bf4a56412622d963a76c4a5a163a870a0ec8
david <mcrobert.david@gmail.com>
2020-08-20 13:47:11 +0100

    StepDefintion added

--- 110820/src/main/java/test/TestRunner.java
+++ 110820/src/main/java/test/TestRunner.java
@@ -1,5 +1,14 @@
 package test;
 
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features="C:\\Users\\david\\git\\repository\\110820\\src\\main\\java\\feature\\oh.feature",
+		glue= {"step"})
 public class TestRunner {
 
 }
