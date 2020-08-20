675797ef89cf81f9dfee932bcea5cca8165b2f0a
david <mcrobert.david@gmail.com>
2020-08-20 15:36:31 +0100

    Added page object.

--- 110820/src/main/java/step/StepDefinition.java
+++ 110820/src/main/java/step/StepDefinition.java
@@ -1,6 +1,24 @@
 package step;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.en.Given;
+import page.PageObject;
 
 public class StepDefinition {
 
+	WebDriver driver;
+	PageObject pageObject;
+	private String country;
+	
+	@Given("^I provide a nationality of \"([^\"]*)\"$")
+	public void i_provide_a_nationality_of_Japan(String country) {
+		this.country=country;
+		pageObject = new PageObject(driver);
+		System.setProperty("webdriver.chrome.driver", "D:\\drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+	    System.out.println("balls");
+
+	}
 }
